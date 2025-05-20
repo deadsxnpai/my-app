5f1c0be9eed0f103f87f83bf25037efaf949f7cb
deadsxnpai <deadsxnpai@gmail.com>
2025-05-20 10:22:45 +0300

    feat: added cards on result page

--- src/pages/results-page/ui/page.tsx
+++ src/pages/results-page/ui/page.tsx
@@ -1,36 +1,62 @@
-import React from "react";
-import { MainLayout } from "@/shared/layouts";
-import cls from "./styles.module.less";
+import React from 'react';
+import { MainLayout } from '@/shared/layouts';
+import cls from './styles.module.less';
 
-import { VStack } from "@/shared/ui/Stack";
-import { recommendMinors } from "@/features/recommened-minors";
-import { useSelector } from "react-redux";
-import { Card, Text } from "@/shared";
+import { HStack, VStack } from '@/shared/ui/Stack';
+import { recommendMinors } from '@/features/recommened-minors';
+import { useSelector } from 'react-redux';
+import { Card, Text } from '@/shared';
+import TrackCard from '@/entities/tracks/ui/TrackCard';
 const ResultPage = () => {
-  const skillsScores = useSelector((state: any) => state.profile.skillsScores);
-  const recommendedMinors = recommendMinors(skillsScores);
-  return (
-    <MainLayout className={cls.main}>
-      <Card padding="16">
-        <Text
-          color="white"
-          size="m"
-          title={`Вам подходят следующие элективы:`}
-        />
-      </Card>
-      <VStack className={cls.container}>
-        {recommendedMinors.length > 0 ? (
-          recommendedMinors.map((minor) => (
-            <Card padding="16">
-              <Text color="white" size="m" text={`${minor.name}`} />
-            </Card>
-          ))
-        ) : (
-          <div>Нет подходящих майноров</div>
-        )}
-      </VStack>
-    </MainLayout>
-  );
+	const skillsScores = useSelector((state: any) => state.profile.skillsScores);
+	const recommendedMinors = recommendMinors(skillsScores);
+	return (
+		<MainLayout className={cls.main}>
+			<Card padding='16'>
+				<Text
+					color='white'
+					size='m'
+					title={`Вам подходят следующие элективы:`}
+				/>
+			</Card>
+			<HStack gap='4'>
+				{recommendedMinors.length > 0 ? (
+					// recommendedMinors.map((minor) => (
+					//   <Card padding="16">
+					//     <Text color="white" size="m" text={`${minor.name}`} />
+					//   </Card>
+					// ))
+					recommendedMinors.map((minor) => (
+						<Card padding='16'>
+							<TrackCard
+								key={minor.name}
+								name={minor.name}
+								type='Трек'
+								role='student'
+								is_visible={true}
+								is_signed={false}
+								is_active={true}
+								id={minor.name}
+								handleNavigate={() => {}}
+								nameSpec={{
+									namespec: '',
+									department_name: '',
+									nameprof: '',
+									group: '',
+									course: '',
+									eform: '',
+									elevel: '',
+									recordbook: '',
+								}}
+							/>
+						</Card>
+					))
+				) : (
+					<div>Нет подходящих майноров</div>
+				)}
+			</HStack>
+		</MainLayout>
+	);
 };
 
 export default ResultPage;
